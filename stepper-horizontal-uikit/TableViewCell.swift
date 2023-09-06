762f31fecefde9f8577b313a1d68f743d888a09b
Aldino Efendi <aldino.efendi@ist.id>
2023-09-06 10:17:41 +0700

    Initial Commit

---
+++ stepper-horizontal-uikit/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  stepper-horizontal-uikit
+//
+//  Created by Aldino Efendi on 2023-09-04.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
